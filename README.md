7f98e958afbe80d82dae5b31c3d29179606a5934
Kenneth Reitz <me@kennethreitz.org>
2017-05-04 21:00:54 -0400

    updates

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # flask-common
-A Flask extension with lots of common time-savers (file-serving, favicons, etc). 
+A Flask extension with lots of common time-savers (file-serving, favicons, etc).
+
+
+## Nicities
+
+- `X-Powered-By=Flask`
+- Favicon support.
+
+## Web Server: Gunicorn
+
+- `WEB_CONCURRENCY`
+-  `PORT`
+
+## File Server: WhiteNoise
---
+++ flask_common.py
@@ -0,0 +1,117 @@
+import os
+import multiprocessing
+
+import crayons
+import maya
+from flask import request, current_app, url_for
+from gunicorn import util
+from gunicorn.app.base import Application
+from whitenoise import WhiteNoise
+
+# Find the stack on which we want to store the database connection.
+# Starting with Flask 0.9, the _app_ctx_stack is the correct one,
+# before that we need to use the _request_ctx_stack.
+try:
+    from flask import _app_ctx_stack as stack
+except ImportError:
+    from flask import _request_ctx_stack as stack
+
+
+# \
+#  \ji
+#  /.(((
+#  (,/"(((__,--.
+#     \  ) _( /{
+#      !|| " :||
+#      !||   :||
+#       '''   '''
+
+# Gunicorn Stuff
+# --------------
+
+def number_of_gunicorn_workers():
+    if not 'WEB_CONCURRENCY' in os.environ:
+        return (multiprocessing.cpu_count() * 2) + 1
+    else:
+        return os.environ['WEB_CONCURRENCY']
+
+class WSGIApp(Application):
+
+    def __init__(self, application, options={}):
+        """ Construct the Application. Default gUnicorn configuration is loaded """
+
+        self.application = application
+        self.usage = None
+        self.callable = None
+        self.options = options
+        self.prog = None
+        self.do_load_config()
+
+    def init(self, parser, opts, args):
+        """ Apply our custom settings """
+
+        cfg = {}
+        for k, v in self.options.items():
+            if k.lower() in self.cfg.settings and v is not None:
+                cfg[k.lower()] = v
+        return cfg
+
+    def load(self):
+        """ Attempt an import of the specified application """
+
+        if isinstance(self.application,str):
+            return util.import_app(self.application)
+        else:
+            return self.application
+
+class GunicornServer(object):
+
+    def __init__(self, app, **options):
+        """ Construct our application """
+
+        self.app = WSGIApp(app, options)
+
+    def run(self):
+        """ Run our application """
+
+        self.app.run()
+
+
+# Common Stuff
+# ------------
+
+class Common(object):
+    """Flask-Common."""
+    def __init__(self, app=None):
+        self.app = app
+        if app is not None:
+            self.init_app(app)
+
+    def init_app(self, app):
+        """Initializes the Flask application with Common."""
+        if not 'COMMON_FILESERVER_DISALBED' in app.config:
+            with app.test_request_context() as c:
+                app.wsgi_app = WhiteNoise(app.wsgi_app, root=url_for('static', filename='')[1:])
+
+        @app.before_request
+        def before_request_callback():
+            request.start_time = maya.now()
+
+        @app.after_request
+        def after_request_callback(response):
+            response.headers['X-Powered-By'] = 'Flask'
+            response.headers['X-Processed-Time'] = maya.now().epoch - request.start_time.epoch
+            return response
+
+
+    def serve(self, workers=None):
+        """Serves the Flask application."""
+        if self.app.debug:
+            print crayons.yellow('Booting Flask development server...')
+            self.app.run()
+
+        else:
+            print crayons.yellow('Booting Gunicorn...')
+            server = GunicornServer(self.app, workers=workers or number_of_gunicorn_workers())
+            server.run()
+
